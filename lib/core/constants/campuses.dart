26bc20dbb06c47a55648124c1c5d2523a2f27800
Huzaifa Qureshi <198439864+muhammad-huzaifa-qureshi@users.noreply.github.com>
2026-03-08 16:27:56 +0500

    campus list created

---
+++ lib/core/constants/campuses.dart
@@ -0,0 +1,12 @@
+class Campuses {
+  static const List<String> list = [
+    "Islamabad E9 Campus",
+    "Islamabad H11 Campus",
+    "Multan Campus",
+    "Kamra Campus",
+    "Karachi Campus",
+    "Kharian Campus",
+    "Karachi Campus",
+    "Bahu Campus",
+  ];
+}
