5acd8821b336a6b7052dba717775062c6c56b54f
Huzaifa Qureshi <198439864+muhammad-huzaifa-qureshi@users.noreply.github.com>
2026-04-12 23:49:19 +0500

    2.6.6

--- lib/core/constants/business_constants.dart
+++ lib/core/constants/business_constants.dart
@@ -42,5 +42,5 @@ class BusinessConstants {
   static const responseFetchLimit = 10;
 
   // version
-  static const appCurrentVersion = "2.6.5 (13)";
+  static const appCurrentVersion = "2.6.6 (14)";
 }
